1cd25399d2f51055a16f8800c584175d7665dc2d
et0305 <beatch234@gmail.com>
2022-11-11 23:37:59 +0800

    version 4: update index.py

--- index.py
+++ index.py
@@ -17,21 +17,19 @@ def index():
 
 @app.route("/read_firebase", methods=["GET", "POST"])
 def read_firebase():
-    cond = request.form["course"]
-    db = firestore.client()
-    collection_ref = db.collection("111")
-    docs = collection_ref.get()
-    result = ""
-    for doc in docs:
-        dict = doc.to_dict()
-        if cond in dict["Course"]:
-            #print("{}老師開的{}課程,每週{}於{}上課".format(dict["Leacture"], dict["Course"],  dict["Time"],dict["Room"]))
-            result += dict["Leacture"] + "老師開的" + dict["Course"] + "課程,每週"
-            result += dict["Time"] + "於" + dict["Room"] + "上課\n"
-        else:
-            result += "抱歉，查無相關條件的選修課程"
-            break
-    print(result)
-
-#if __name__ == "__main__":
-    #app.run()
+    if request.method == "POST":
+        cond = request.form["course"]
+        db = firestore.client()
+        collection_ref = db.collection("111")
+        docs = collection_ref.get()
+        result = ""
+        for doc in docs:
+            dict = doc.to_dict()
+            if cond in dict["Course"]:
+                #print("{}老師開的{}課程,每週{}於{}上課".format(dict["Leacture"], dict["Course"],  dict["Time"],dict["Room"]))
+                result += dict["Leacture"] + "老師開的" + dict["Course"] + "課程,每週"
+                result += dict["Time"] + "於" + dict["Room"] + "上課\n"
+            else:
+                result += "抱歉，查無相關條件的選修課程"
+                break
+        print(result)
